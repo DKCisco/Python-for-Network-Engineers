075b5369a489f2dfb93fb5355c0f9352015cea5f
DKCisco <73546877+DKCisco@users.noreply.github.com>
2022-08-01 14:02:47 -0600

    Add files via upload

---
+++ function_recap.py
@@ -0,0 +1,14 @@
+def test_example():
+    print('This is a test function!')
+
+test_example()
+
+def nameprinter(name):
+    print(f"NAME PRINTED: {name}")
+
+nameprinter("John")
+
+def adder(num1, num2,):
+    print(num1 + num2)
+
+adder(2, 3)
