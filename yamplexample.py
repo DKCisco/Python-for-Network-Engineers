b68b7f228f49decfe1a2768fb51dac6e3aa8cd56
DKCisco <73546877+DKCisco@users.noreply.github.com>
2022-08-05 15:09:38 -0600

    Add files via upload

---
+++ yamplexample.py
@@ -0,0 +1,17 @@
+import yaml
+from rich import print as rprint
+
+def pull_yaml():
+  """
+  Function to pull yaml data
+  """
+  config_data = yaml.safe_load(open("r1.yaml"))
+  bgp_stuff = config_data["BGP"]
+  peers = bgp_stuff["peers"]
+  for peer in peers:
+    nbor = peer["neighbor"]
+    asnumber = peer["peer_asn"]
+    print(f"The neighbor {nbor} has an ASN of {asnumber}")
+
+
+pull_yaml()
