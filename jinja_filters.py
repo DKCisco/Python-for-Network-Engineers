a7d47292b944290b6f4192a1a65e36e27c01e0a0
DKCisco <73546877+DKCisco@users.noreply.github.com>
2022-08-15 14:02:26 -0600

    Add files via upload

---
+++ jinja_filters.py
@@ -0,0 +1,16 @@
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+def generate_config():
+    """
+    Load in host variables.
+    Generate config from template
+    """
+
+    my_vars = yaml.safe_load(open("R1_revised.yml"))
+    env = Environment(loader = FileSystemLoader("./templates"), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template("snmp.j2")
+    configuration = template.render(my_vars)
+    print(configuration)
+
+generate_config()
