e7521cfbf5843b1e8c072710253f0d59f4db9278
DKCisco <dillan.k.craig@gmail.com>
2022-08-25 18:22:09 +0000

    get_example.py

---
+++ get_example.py
@@ -0,0 +1,15 @@
+import json
+from urllib import response
+import httpx
+from rich import print as rprint
+
+headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+my_url = "https://gorest.co.in/public/v2/users"
+
+def pull_info(url):
+    with httpx.Client() as client:
+        response = json.loads(client.get(url, headers=headers).text)
+        return response
+
+results = pull_info(my_url)
+rprint(results)
