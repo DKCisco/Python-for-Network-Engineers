ec6ebc968022b5519c1aefc84bfc533cce2b493d
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 17:43:47 +0000

    PEP 8 - Style guide

---
+++ example.py
@@ -0,0 +1,12 @@
+# PEP 8 – Style Guide for Python Code
+# Classes should be CamelCase
+# Functions should be lowercase
+
+from my_maths import adder, subtractor
+
+MY_THRESHOLD = 0
+
+class TestNetworkState:
+    pass
+
+
