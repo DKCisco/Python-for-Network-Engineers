e1c3881a61014abf1f5b2302d348328a2de29f46
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 19:25:14 +0000

    import examples

---
+++ import_example.py
@@ -0,0 +1,11 @@
+#Standard library imports
+import getpass
+import os
+
+#Third party imports
+from rich import print as rprint
+from scrapli.driver.core import IOSXEDriver
+
+#Local imports
+from my_maths import the_multiplier
+
