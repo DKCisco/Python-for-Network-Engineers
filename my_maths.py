9245de116cbeca4551c9e3fd9da5ef9a78763968
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 17:43:42 +0000

    PEP 8 - Style guide

---
+++ my_maths.py
@@ -0,0 +1,12 @@
+# PEP 8 – Style Guide for Python Code
+# Classes should be CamelCase
+# Functions should be lowercase
+
+def adder(a,b):
+    return a + b
+
+def subtractor(a,b):
+    return a - b
+
+def the_multiplier(a,b):
+    return a * b
