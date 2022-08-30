69ce1249ffb4e9940889888528ee985fe82fc29e
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 19:05:50 +0000

    comment example

---
+++ comment_example.py
@@ -0,0 +1,8 @@
+print ("My name is John") #print my name
+
+#print my surname
+print("My surname is McGovern")
+
+# this a comment goes on and on and on and on and on and on and on and on and on 
+# and on and on and on and on and on and on and on and onand on and on and on 
+# and on and on and on and on and on
