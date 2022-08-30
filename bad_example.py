52acc7676a36fcb3394a8a9e9c0ef44858f4284b
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 20:34:14 +0000

    example of what no to do

---
+++ bad_example.py
@@ -0,0 +1,16 @@
+"""
+An example of what not to do for pylint
+
+"""
+import ntp_stuff
+import os
+
+l = "7"
+
+def CAPITALFUNCTION():
+    print("bad function")
+
+print("This is a really really really really really really really really really really really really really really really really really really really long line")
+
+class badclass:
+    pass
