cdaeabf967514db321c8d989a8736a8512f4f899
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 16:27:08 +0000

    devnet sandbox

---
+++ get_devnet.py
@@ -0,0 +1,16 @@
+import json
+from urllib import response
+import httpx
+from rich import print as rprint
+
+headers = {'Accept': 'application/yang-data+json', 'Content-Type': 'application/yang-data+json'}
+my_url = "https://sandbox-iosxe-latest-1.cisco.com:443/restconf/data/native"
+
+def pull_info(url):
+    with httpx.Client(verify=False) as client:
+        response = client.get(url, headers=headers, auth=("developer", "C1sco12345"))
+        return response.text
+
+results = pull_info(my_url)
+rprint(results)
+
