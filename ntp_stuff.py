32d9ae122ccb5ead712cab575d0d97b5e87a96be
DKCisco <dillan.k.craig@gmail.com>
2022-08-30 20:12:26 +0000

    structuring code

---
+++ ntp_stuff.py
@@ -0,0 +1,12 @@
+def ntp_server(ntp ="99.99.99.99"):
+    print(f"NTP SERVER: {ntp}")
+
+def vlan_caller(vlan):
+    print(f"The VLAN is called {vlan}")
+
+
+
+if __name__ == "__main__":
+    ntp_server("1.2.3.4")
+
+#print(f"{__name__}")
