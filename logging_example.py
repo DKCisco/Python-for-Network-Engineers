31d820ccd6abb98a66987fa0af58923b838c0e6b
DKCisco <dillan.k.craig@gmail.com>
2022-09-14 01:20:24 +0000

    updated debugging/logging files

--- logging_example.py
+++ logging_example.py
@@ -1,19 +1,21 @@
-from urllib import response
 import logging
-from scrapli.driver.core import JunosDriver
+#from scrapli.driver.core import JunosDriver
+from scrapli.driver.core import IOSXEDriver
 
-logging.basicConfig(filename="newlogsblah.txt", level=logging.DEBUG)
+logging.basicConfig(filename="newlogsconfig.txt", level=logging.DEBUG)
 
 def test_func():
-    with JunosDriver(
-        host="192.168.50.21",
+    with IOSXEDriver(
+        host="192.168.50.22",
         auth_username = "ds_local_netadmin",
         auth_password = "XIEY;R~pBfi/1YmF@@VC",
         auth_secondary = "1ekVJm2^keW|gJ~hZR~r",
         auth_strict_key = False,
         ssh_config_file = True,
     ) as conn:
-        response = conn.send_command("show version")
+        response = conn.send_configs(["router ospf 51"])
         return response.result
+
 if __name__ == "__main__":
-    test_func()
+    results = test_func()
+    print(results)
