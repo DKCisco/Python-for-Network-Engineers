2f87ef1a85654cc9d98b0eee440a9c02bc5f195a
DKCisco <dillan.k.craig@gmail.com>
2022-09-14 15:40:41 +0000

    exceptions overview 1

---
+++ exceptions_overview.py
@@ -0,0 +1,14 @@
+try:
+    x = "Lalo"
+    x + 5
+    #print(x)
+except NameError:
+    print("Can't print this because of a name error. The value is not defined")
+except Exception:
+    print("Some unknown exception")   
+
+
+
+y = "john"
+print(y)
+
