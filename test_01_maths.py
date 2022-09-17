dd10b6273a02a05b4bc3111316683eab06b598c8
DKCisco <dillan.k.craig@gmail.com>
2022-09-17 22:33:50 +0000

    unit test module 2

---
+++ test_01_maths.py
@@ -0,0 +1,21 @@
+from audioop import mul
+import unittest
+from unit_test_module_1 import multiplier, subtractor
+
+class TestMaths(unittest.TestCase):
+
+    def test_multiplier(self):
+        total = multiplier(2, 4)
+        self.assertEqual(total, 8)
+
+    def test_subtractor(self):
+        total = subtractor(7, 4)
+        self.assertEqual(total, 3)
+    
+    def test_blah(self):
+        my_list = [2,4,7,8]
+        my_num = multiplier(2, 3)
+        self.assertIn(my_num, my_list)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ test_02_ipvzero.py
@@ -0,0 +1,21 @@
+from audioop import mul
+import unittest
+from unit_test_module_1 import multiplier, subtractor
+
+class TestMaths(unittest.TestCase):
+
+    def test_multiplier(self):
+        total = multiplier(2, 5)
+        self.assertEqual(total, 10)
+
+    def test_subtractor(self):
+        total = subtractor(8, 2)
+        self.assertEqual(total, 12)
+    
+    def test_blah(self):
+        my_list = [2,4,7,8]
+        my_num = multiplier(2, 1)
+        self.assertIn(my_num, my_list)
+
+if __name__ == "__main__":
+    unittest.main()
